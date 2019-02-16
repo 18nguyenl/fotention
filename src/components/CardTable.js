732f07ea02b87231b5e325d23e9a9814c6dd6c7f
Long Nguyen <dystraxus@iMac.local>
2019-02-16 01:45:20 -0600

    create janky TransitionGroup animation

--- src/components/CardTable.js
+++ src/components/CardTable.js
@@ -1,71 +1,93 @@
-import React, {Component} from 'react';
-import "../CardTable.css"
-import {TweenMax, TweenLite, Power2, TimelineLite, CSSPlugin} from 'gsap/all'
-import {companies} from "../data/companies.js"
+import React, { Component } from "react";
+import "../CardTable.css";
+import { TweenMax, TweenLite, Power2, TimelineLite, CSSPlugin } from "gsap/all";
+import { Transition, TransitionGroup } from "react-transition-group";
+import { companies } from "../data/companies.js";
 
 export default class CardTable extends Component {
-    constructor(props) {
-        super(props);
-        this.myTween = new TimelineLite({paused: true});
-        this.myElements = [];
-    }
-    
-    componentDidMount() {
-        this.myTween.staggerFrom(this.myElements, 0.7, {x: -60, autoAlpha: 0, ease: Power2.Out}, 0.1);
-        this.myTween.play();
-    }
+  constructor(props) {
+    super(props);
+    this.myTween = new TimelineLite({ paused: true });
+    this.myElements = [];
+  }
 
-    // setRef (index, ref) {
-    //     return this.myElements[index] = ref;
-    // }
-    
-    render() {
-        // const companies = this.state.cards.splice();
-        // companies.push(<Card header= "Wopa" body= "$48 = 400p"/>)
-        // companies.push(<Card header= "Quartz" body= "25% off = 200p"/>)
-        // companies.push(<Card header= "Yeetus" body= "New shoes = 400"/>)
-        // companies.push(<Card header= "Skype" body= "BOGO"/>)
-        this.myTween.kill().clear().pause(0);
-        return(
-        <div className="card-table">
-            {companies.map((element, index) => {
-                return <div
-                    key={index}
-                    // className= "card"
-                    ref={div => this.myElements[index] = div}
-                >
-                <Card 
-                    // innerRef={this.setRef.bind(this)}
-                    header={element.header}
-                    body={element.body}
+  componentDidMount() {
+    // this.myTween.staggerFrom(
+    //   this.myElements,
+    //   1,
+    //   { x: -60, autoAlpha: 0, ease: Power2.Out },
+    //   0.2
+    // );
+    // this.myTween.play();
+  }
+
+  // setRef (index, ref) {
+  //     return this.myElements[index] = ref;
+  // }
+
+  render() {
+    // const companies = this.state.cards.splice();
+    // companies.push(<Card header= "Wopa" body= "$48 = 400p"/>)
+    // companies.push(<Card header= "Quartz" body= "25% off = 200p"/>)
+    // companies.push(<Card header= "Yeetus" body= "New shoes = 400"/>)
+    // companies.push(<Card header= "Skype" body= "BOGO"/>)
+    this.myTween
+      .kill()
+      .clear()
+      .pause(0);
+    return (
+      <div>
+        <TransitionGroup className="card-table">
+          {companies.map((element, index) => {
+            return (
+              <Transition
+                key={index}
+                // className= "card"
+                ref={div => (this.myElements[index] = div)}
+                timeout={1000}
+                // style={{"transitionDelay": `${index * .05}s`}}
+                mountOnEnter
+                unmountOnExit
+                appear={true}
+                onEnter={node => {
+                  TweenMax.from(node, 1, {
+                    x: -60,
+                    autoAlpha: 0,
+                    ease: Power2.Out,
+                    delay: index * 0.14, // janky solution
+                  });
+                }}
+              >
+                <Card
+                  // innerRef={this.setRef.bind(this)}
+                  header={element.header}
+                  body={element.body}
                 />
-                </div> 
-            })}
-        </div>
-        );
-    }
+              </Transition>
+            );
+          })}
+        </TransitionGroup>
+      </div>
+    );
+  }
 }
 
 export class Card extends Component {
-    constructor(props) {
-        super(props);
-        this.myElement = null;
-    }
+  constructor(props) {
+    super(props);
+    this.myElement = null;
+  }
 
-    componentDidMount() {
-        // this.props.innerRef(this.props.key, this.myElement);
-    }
+  componentDidMount() {
+    // this.props.innerRef(this.props.key, this.myElement);
+  }
 
-    render() {
-        return(
-            <div className = "card">
-                <h2 className = "card-header">
-                    {this.props.header}
-                </h2>
-                <div className = "card-body">
-                    {this.props.body}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="card">
+        <h2 className="card-header">{this.props.header}</h2>
+        <div className="card-body">{this.props.body}</div>
+      </div>
+    );
+  }
 }
